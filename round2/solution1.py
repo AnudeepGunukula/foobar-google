4938b2ca6f0c2b9afa5871e537854b38fa30c85a
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-08-13 18:44:31 +0530

    Update solution1.py

--- round2/solution1.py
+++ round2/solution1.py
@@ -1,1 +1,33 @@
-
+def f1(n):
+    li=[]
+    summ=0
+    x=1
+    count=0
+    while summ+x<=n:
+        summ+=x
+        x*=2
+        count+=1
+    return count
+    
+def f2(n):
+    x=0
+    y=1 
+    li=[]
+    summ=0
+    while True:
+        z=x+y 
+        x=y 
+        y=z
+        li.append(y)
+        if summ+z>=n:
+            break
+        summ+=z
+    return len(li)
+        
+        
+        
+def solution(total_lambs):
+        n = total_lambs
+        return f2(n)-f1(n)
+    
+print(solution(143))
